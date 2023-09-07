0fbcd26ec71614b752df7f2d077c97ea0c5dbdb2
Sergio José Santizo Barraza <144158305+Shesho04@users.noreply.github.com>
2023-09-07 09:34:40 -0600

    Add files via upload

---
+++ TS5-SJSB_1018723.py
@@ -0,0 +1,22 @@
+#Trabajo Supervisado de Introducción a la Programación, sección: 17
+#Fecha de creación: 7/09/2023
+#Sergio José Santizo Barraza 1018723
+
+#Objetivo: Crear un programa que solicite los datos del usuario, los almacene en variables y lo muestre en la pantalla.
+#Entrada: Nombre, Edad, Carrera, Carné.
+#Procesos principales
+print("Mi segundo programa")
+sNombre = input("Ingrese su nombre: ")
+print("Nombre: " + sNombre)
+
+sEdad = input("Ingrese su edad: ")
+print("edad: " + sEdad)
+
+sCarrera = input("Ingrese su carrera: ")
+print("carrera: " + sCarrera)
+
+sCarné = input("Ingrese su carné: ")
+print("carné: " + sCarné)
+
+#Salida
+print("Mi nombre es " + sNombre + ", tengo " + sEdad + " años de edad" + ", mi carrera es " + sCarrera + " y mi número de carné es" + sCarné)
